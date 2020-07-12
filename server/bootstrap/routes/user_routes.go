444e4c8a0a9f73b4c34f63f7547b332f7656bc54
syaikhul hadi <my.ant2008@gmail.com>
2020-07-12 09:54:26 +0700

    add middleware in user

--- server/bootstrap/routes/user_routes.go
+++ server/bootstrap/routes/user_routes.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"github.com/labstack/echo"
 	"qibla-backend/server/handlers"
+	"qibla-backend/server/middleware"
 )
 
 type UserRoutes struct {
@@ -12,8 +13,10 @@ type UserRoutes struct {
 
 func (route UserRoutes) RegisterRoute() {
 	userHandler := handlers.UserHandler{Handler: route.Handler}
+	jwtMiddleware := middleware.JwtVerify{UcContract:route.Handler.UseCaseContract}
 
 	userRoute := route.RouteGroup.Group("/user")
+	userRoute.Use(jwtMiddleware.JWTWithConfig)
 	userRoute.GET("", userHandler.Browse)
 	userRoute.GET("/:id", userHandler.Read)
 	userRoute.PUT("/:id", userHandler.Edit)
