da7c6fc1290b95340dd04e56ad1518a697ac1ef3
syaikhul hadi <my.ant2008@gmail.com>
2020-09-19 09:18:56 +0700

    log

--- db/repositories/actions/transaction_repository.go
+++ db/repositories/actions/transaction_repository.go
@@ -2,6 +2,7 @@ package actions
 
 import (
 	"database/sql"
+	"fmt"
 	"qibla-backend/db/models"
 	"qibla-backend/db/repositories/contracts"
 	"qibla-backend/helpers/datetime"
@@ -89,6 +90,8 @@ func (repository TransactionRepository) EditDueDate(ID, dueDate, updatedAt strin
 }
 
 func (repository TransactionRepository) EditStatus(ID, paymentStatus, paidDate, updatedAt string, tx *sql.Tx) (err error) {
+	fmt.Println(updatedAt)
+	fmt.Println(datetime.StrParseToTime(updatedAt, time.RFC3339))
 	statement := `update "transactions" set "payment_status"=$1, "paid_date"=$2, "updated_at"=$3 where "id"=$3 returning "id"`
 	_, err = tx.Exec(
 		statement,
