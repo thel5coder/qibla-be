700b65e1666c56761689e103ed648d5c16850aab
syaikhul hadi <my.ant2008@gmail.com>
2020-11-03 23:55:02 +0700

    menu user permission model

--- db/models/MenuUserPermission.go
+++ db/models/MenuUserPermission.go
@@ -1,6 +1,6 @@
 package models
 
-type MenuPermissionUser struct {
+type MenuUserPermission struct {
 	ID               string `db:"id"`
 	MenuID           string `db:"menu_id"`
 	MenuPermissionID string `db:"menu_permission_id"`
