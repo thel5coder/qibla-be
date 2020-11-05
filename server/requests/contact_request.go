0bc38e346fc44263142b4399c741d463cb9bfb07
syaikhul hadi <my.ant2008@gmail.com>
2020-11-05 21:14:56 +0700

    hot fix contact

--- server/requests/contact_request.go
+++ server/requests/contact_request.go
@@ -3,25 +3,25 @@ package requests
 type ContactRequest struct {
 	BranchName           string `json:"branch_name"`
 	TravelAgentName      string `json:"travel_agent_name"`
-	Address              string `json:"address"`
+	Address              string `json:"address" validate:"required"`
 	Longitude            string `json:"longitude"`
 	Latitude             string `json:"latitude"`
-	AreaCode             string `json:"area_code"`
-	PhoneNumber          string `json:"phone_number"`
+	AreaCode             string `json:"area_code" validate:"required"`
+	PhoneNumber          string `json:"phone_number" validate:"required"`
 	SKNumber             string `json:"sk_number"`
-	SKDate               string `json:"sk_date" validate:"date"`
+	SKDate               string `json:"sk_date"`
 	Accreditation        string `json:"accreditation"`
-	AccreditationDate    string `json:"accreditation_date" validate:"date"`
-	DirectorName         string `json:"director_name"`
+	AccreditationDate    string `json:"accreditation_date"`
+	DirectorName         string `json:"director_name" validate:"required"`
 	DirectorContact      string `json:"director_contact"`
 	PicName              string `json:"pic_name"`
 	PicContact           string `json:"pic_contact"`
-	Logo                 string `json:"logo"`
+	Logo                 string `json:"logo" validate:"required"`
 	VirtualAccountNumber string `json:"virtual_account_number"`
 	AccountNumber        string `json:"account_number"`
 	AccountName          string `json:"account_name"`
 	AccountBankName      string `json:"account_bank_name"`
 	AccountBankCode      string `json:"account_bank_code"`
-	Email                string `json:"email"`
+	Email                string `json:"email" validate:"required"`
 	IsZakatPartner       bool   `json:"is_zakat_partner"`
 }
