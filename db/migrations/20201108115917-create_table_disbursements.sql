46f249983020e4caa91df6a8c18a85e7e6b0fce0
syaikhul hadi <my.ant2008@gmail.com>
2020-11-08 12:11:40 +0700

    add field disbursement

---
+++ db/migrations/20201108115917-create_table_disbursements.sql
@@ -0,0 +1,22 @@
+-- +migrate Up
+CREATE TABLE IF NOT EXISTS "disbursements"
+(
+    "id"                char(36) PRIMARY KEY NOT NULL DEFAULT (uuid_generate_v4()),
+    "transaction_id"    char(36)             NOT NULL,
+    "total"             float4,
+    "status"            status_transaction_enum,
+    "disbursment_type"  disbursement_type_enum,
+    "start_period"      timestamp,
+    "end_period"        timestamp,
+    "account_number"    varchar(20)          NOT NULL,
+    "account_name"      varchar(50)          NOT NULL,
+    "account_bank_name" varchar(20)          NOT NULL,
+    "account_bank_code" varchar(5)           NOT NULL,
+    "created_at"        timestamp            NOT NULL,
+    "updated_at"        timestamp            NOT NULL,
+    "disburse_at"       timestamp,
+    "deleted_at"        timestamp
+);
+
+-- +migrate Down
+DROP TABLE IF EXISTS "disbursements";
