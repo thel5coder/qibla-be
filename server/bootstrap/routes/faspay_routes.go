24448d7ad8d92a2c6f4fc0606772d440524ce3ae
syaikhul hadi <my.ant2008@gmail.com>
2020-11-09 00:03:12 +0700

    hot fix route faspay route

--- server/bootstrap/routes/faspay_routes.go
+++ server/bootstrap/routes/faspay_routes.go
@@ -15,7 +15,6 @@ func(route FaspayRoutes) RegisterRoute(){
 	handler := handlers.FasPayHandler{Handler:route.Handler}
 	jwtMiddleware := middleware.JwtVerify{UcContract:route.Handler.UseCaseContract}
 
-
 	faspayRoute := route.RouteGroup.Group("/faspay_api")
 	faspayRoute.Use(jwtMiddleware.JWTWithConfig)
 	faspayRoute.GET("/get-payment-method",handler.GetLisPaymentMethods)
