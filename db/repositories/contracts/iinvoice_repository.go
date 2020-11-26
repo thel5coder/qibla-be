5541ce1b75c4b2af12e1052401d120ba89bf4e8d
ahmadshobirin <ahmadshobirin.dev@gmail.com>
2020-11-26 23:37:55 +0700

    remove model and repo repository

--- db/repositories/contracts/iinvoice_repository.go
+++
@@ -1,8 +0,0 @@
-package contracts
-
-import "qibla-backend/db/models"
-
-// IInvoiceRepository ...
-type IInvoiceRepository interface {
-	Browse(order, sort string, limit, offset int) (data []models.Invoice, count int, err error)
-}
