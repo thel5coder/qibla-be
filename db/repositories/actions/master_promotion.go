53d2c2f646512c3a1b188c14d4ba800e33ef4880
syaikhul hadi <my.ant2008@gmail.com>
2020-12-09 19:12:40 +0700

    hot fix master promotion

--- db/repositories/actions/master_promotion.go
+++ db/repositories/actions/master_promotion.go
@@ -43,7 +43,7 @@ func(repository MasterPromotionRepository) scanRow(row *sql.Row) (res models.Mas
 func (repository MasterPromotionRepository) Browse(filters map[string]interface{}, order, sort string, limit, offset int) (data []models.MasterPromotion, count int, err error) {
 	filterStatement := master_promotion_helper.ParseFilterStatement(filters)
 
-	statement := masterPromotionSelect+` from "promotion_packages" where "deleted_at" is null `+filterStatement+` order by ` + order + ` ` + sort + ` limit $1 offset $2`
+	statement := masterPromotionSelect+` from "master_promotions" where "deleted_at" is null `+filterStatement+` order by ` + order + ` ` + sort + ` limit $1 offset $2`
 	rows, err := repository.DB.Query(statement, limit, offset)
 	if err != nil {
 		return data, count, err
@@ -58,7 +58,7 @@ func (repository MasterPromotionRepository) Browse(filters map[string]interface{
 		data = append(data, temp)
 	}
 
-	statement = `select count("id") from master_promotion_helper where "deleted_at" is null`+filterStatement
+	statement = `select count("id") from "master_promotions" where "deleted_at" is null`+filterStatement
 	err = repository.DB.QueryRow(statement).Scan(&count)
 	if err != nil {
 		return data, count, err
@@ -68,7 +68,7 @@ func (repository MasterPromotionRepository) Browse(filters map[string]interface{
 }
 
 func (repository MasterPromotionRepository) BrowseAll() (data []models.MasterPromotion,err error){
-	statement := `select * from master_promotion_helper`
+	statement := masterPromotionSelect+` from master_promotions`
 	rows, err := repository.DB.Query(statement)
 	if err != nil {
 		return data, err
@@ -87,7 +87,7 @@ func (repository MasterPromotionRepository) BrowseAll() (data []models.MasterPro
 }
 
 func (repository MasterPromotionRepository) ReadBy(column, value string) (data models.MasterPromotion, err error) {
-	statement := `select * from "promotion_packages" where ` + column + `=$1 and "deleted_at" is null`
+	statement := masterPromotionSelect+` from "master_promotions" where ` + column + `=$1 and "deleted_at" is null`
 	row := repository.DB.QueryRow(statement,value)
 	data,err = repository.scanRow(row)
 
@@ -95,14 +95,14 @@ func (repository MasterPromotionRepository) ReadBy(column, value string) (data m
 }
 
 func (repository MasterPromotionRepository) Edit(input viewmodel.MasterPromotionVm) (res string, err error) {
-	statement := `update master_promotion_helper set name=$1, "slug"=$2, "updated_at"=$3, "is_active"=$4 where "id"=$5 returning "id"`
+	statement := `update "master_promotions" set name=$1, "slug"=$2, "updated_at"=$3, "is_active"=$4 where "id"=$5 returning "id"`
 	err = repository.DB.QueryRow(statement, input.Name, input.Slug, datetime.StrParseToTime(input.UpdatedAt, time.RFC3339), input.IsActive, input.ID).Scan(&res)
 
 	return res, err
 }
 
 func (repository MasterPromotionRepository) Add(input viewmodel.MasterPromotionVm) (res string, err error) {
-	statement := `insert into master_promotion_helper (name,"slug","created_at","updated_at","is_active") values($1,$2,$3,$4,$5) returning "id"`
+	statement := `insert into "master_promotions" (name,"slug","created_at","updated_at","is_active") values($1,$2,$3,$4,$5) returning "id"`
 	err = repository.DB.QueryRow(
 		statement,
 		input.Name,
@@ -116,7 +116,7 @@ func (repository MasterPromotionRepository) Add(input viewmodel.MasterPromotionV
 }
 
 func (repository MasterPromotionRepository) Delete(ID, updatedAt, deletedAt string) (res string, err error) {
-	statement := `update master_promotion_helper set "updated_at"=$1, "deleted_at"=$2 where "id"=$3 returning "id"`
+	statement := `update "master_promotions" set "updated_at"=$1, "deleted_at"=$2 where "id"=$3 returning "id"`
 	err = repository.DB.QueryRow(statement, datetime.StrParseToTime(updatedAt, time.RFC3339), datetime.StrParseToTime(deletedAt, time.RFC3339), ID).Scan(&res)
 
 	return res, err
@@ -124,10 +124,10 @@ func (repository MasterPromotionRepository) Delete(ID, updatedAt, deletedAt stri
 
 func (repository MasterPromotionRepository) CountBy(ID, column, value string) (res int, err error) {
 	if ID == "" {
-		statement := `select count("id") from "promotion_packages" where ` + column + `=$1 and "deleted_at" is null`
+		statement := `select count("id") from "master_promotions" where ` + column + `=$1 and "deleted_at" is null`
 		err = repository.DB.QueryRow(statement, value).Scan(&res)
 	} else {
-		statement := `select count("id") from "promotion_packages" where (` + column + `=$1 and "deleted_at" is null) and "id"<>$2`
+		statement := `select count("id") from "master_promotions" where (` + column + `=$1 and "deleted_at" is null) and "id"<>$2`
 		err = repository.DB.QueryRow(statement, value, ID).Scan(&res)
 	}
 
