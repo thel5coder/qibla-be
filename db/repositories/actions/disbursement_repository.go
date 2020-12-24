02c9b8360d5f79a2e6e3f3032a42d1f8853865e4
syaikhul hadi <my.ant2008@gmail.com>
2020-12-24 12:55:18 +0700

    hot fix query disbursment zakat period filter

--- db/repositories/actions/disbursement_repository.go
+++ db/repositories/actions/disbursement_repository.go
@@ -66,9 +66,11 @@ func (repository DisbursementRepository) Browse(filters map[string]interface{}, 
 	if val, ok := filters["total"]; ok {
 		conditionString += ` AND def."total"::TEXT LIKE '%` + val.(string) + `%'`
 	}
-	//if val, ok := filters["transaction_period"]; ok {
-	//	conditionString += ` AND (def."start_period" < '` + val.(string) + `' or def."end_period" > '`+val.(string)+`')`
-	//}
+
+	if val, ok := filters["transaction_period"]; ok {
+		conditionString += ` AND (def."start_period" < '` + val.(string) + `' or def."end_period" > '`+val.(string)+`')`
+	}
+
 	if val, ok := filters["contact_account_bank_name"]; ok {
 		conditionString += ` AND LOWER(c."account_bank_name") LIKE '%` + strings.ToLower(val.(string)) + `%'`
 	}
